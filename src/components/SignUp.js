f43a02627d4d2b7fe2ad7f5b533d01336330495b
nickcabuno <nickcabuno17@gmail.com>
2021-08-30 22:53:01 -0400

    man

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -70,7 +70,7 @@ const SignUp = () => {
             <label>
               <input
                 name="username"
-                placeholder="username"
+                placeholder="Username"
                 type="text"
                 value={formValues.username}
                 onChange={handleChanges}
@@ -80,7 +80,7 @@ const SignUp = () => {
             <label>
               <input
                 name="password"
-                placeholder="password"
+                placeholder="Password"
                 type="password"
                 value={formValues.password}
                 onChange={handleChanges}
